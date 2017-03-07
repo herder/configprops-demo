143a46ac5299861d4ab8b24fb3b184b2de19c97e
Niklas Herder <niklas.herder@aftonbladet.se>
2017-03-07 08:18:54 +0100

    Simplify and add tests.

--- src/main/kotlin/com/example/ConfigProps.kt
+++ src/main/kotlin/com/example/ConfigProps.kt
@@ -3,8 +3,6 @@ package com.example
 import org.springframework.boot.context.properties.ConfigurationProperties
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
-import org.springframework.context.annotation.Profile
-import org.springframework.util.Assert
 import org.springframework.validation.annotation.Validated
 import java.text.Normalizer
 import javax.validation.constraints.Min
@@ -14,38 +12,15 @@ open class SlugServicePropertiesKotlin(@Min(10) var slugSize: Int = 70,
                                        var normalizerForm: Normalizer.Form = Normalizer.Form.NFD)
 
 @Configuration
-@Profile("java")
-open class SlugServiceConfigurationKotlin {
+open class SlugServiceConfiguration {
 
 
     @Bean
     @ConfigurationProperties(prefix = "slugservice.articles", exceptionIfInvalid = true)
-    open fun slugServiceProperties() = SlugServicePropertiesJava()
-
-
-    @Bean
-    open fun getProps(slugServiceProperties: SlugServicePropertiesJava): String {
-        Assert.isTrue(slugServiceProperties.slugSize >= 10, "slugSize was not >= 10")
-        Assert.isTrue(slugServiceProperties.normalizerForm == Normalizer.Form.NFD, "normalizerForm was not NFD")
-        return "Ok"
-    }
-}
-
-
-@Configuration
-@Profile("kotlin")
-open class SlugServiceConfigurationJava {
-
+    open fun slugServicePropertiesJava() = SlugServicePropertiesJava()
 
     @Bean
     @ConfigurationProperties(prefix = "slugservice.articles", exceptionIfInvalid = true)
-    open fun slugServiceProperties() = SlugServicePropertiesKotlin()
+    open fun slugServicePropertiesKotlin() = SlugServicePropertiesKotlin()
 
-
-    @Bean
-    open fun getProps(slugServiceProperties: SlugServicePropertiesKotlin): String {
-        Assert.isTrue(slugServiceProperties.slugSize >= 10, "slugSize was not >= 10")
-        Assert.isTrue(slugServiceProperties.normalizerForm == Normalizer.Form.NFD, "normalizerForm was not NFD")
-        return "Ok"
-    }
 }
--- src/test/kotlin/com/example/ConfigpropsDemoApplicationTests.kt
+++ src/test/kotlin/com/example/ConfigpropsDemoApplicationTests.kt
@@ -2,15 +2,33 @@ package com.example
 
 import org.junit.Test
 import org.junit.runner.RunWith
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.test.context.junit4.SpringRunner
+import org.springframework.util.Assert
+import java.text.Normalizer
 
 @RunWith(SpringRunner::class)
 @SpringBootTest
-class ConfigpropsDemoApplicationTests {
+class ConfigpropsDemoApplicationTestsJava {
 
-	@Test
-	fun contextLoads() {
-	}
+    @Autowired
+    lateinit var slugServicePropertiesJava: SlugServicePropertiesJava
+
+    @Autowired
+    lateinit var slugServicePropertiesKotlin: SlugServicePropertiesKotlin
+
+    @Test
+    fun testDefaultPropsJava() {
+        Assert.isTrue(slugServicePropertiesJava.slugSize >= 10, "slugSize was not >= 10")
+        Assert.isTrue(slugServicePropertiesJava.normalizerForm == Normalizer.Form.NFD, "normalizerForm was not NFD")
+    }
+
+
+    @Test
+    fun testDefaultPropsKotlin() {
+        Assert.isTrue(slugServicePropertiesKotlin.slugSize >= 10, "slugSize was not >= 10")
+        Assert.isTrue(slugServicePropertiesKotlin.normalizerForm == Normalizer.Form.NFD, "normalizerForm was not NFD")
+    }
 
 }
